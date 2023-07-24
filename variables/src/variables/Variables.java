5e08c1e24f6d35a085ec148adcb3cb7255e65196
Jorge Gómez Gerling <jorgegomezgerling@gmail.com>
2023-07-23 21:07:35 -0300

    arreglando

--- variables/src/variables/Variables.java
+++ variables/src/variables/Variables.java
@@ -3,7 +3,14 @@ package variables;
 public class Variables {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int numero = 5;
+		String nombre = "Jorge";
+		char letra = 'j';
+		double temperatura = 27.5;
+		boolean siono = false;
+		long largo = 123456;
+		
 
 	}
 
